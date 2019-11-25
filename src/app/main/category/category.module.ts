0b78736885e568d7e5144164b14c70bc3963fcdd
achauhan <achauhan@inetco.com>
2019-11-25 23:57:39 +0530

    fixed service import

--- src/app/main/category/category.module.ts
+++ src/app/main/category/category.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './category.component';
-import { CategoryService } from '../service/category.service';
-
-
 
 @NgModule({
   declarations: [CategoryComponent],
   imports: [
     CommonModule
   ],
-  providers: [CategoryService],
 })
 export class CategoryModule { }
